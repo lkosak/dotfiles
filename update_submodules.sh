68eb216630bc07a37d1ed04e04c8000356ee0ac0
Lou Kosak <lou.kosak@airbnb.com>
2018-09-29 00:17:52 +0900

    fix submodule thing

--- update_submodules.sh
+++ update_submodules.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git submodule init # needed for first run; should no-op if already run
-git submodule update # needed for first run; should no-op if already run
+git submodule update -i # needed for first run; should no-op if already run
 git submodule foreach git pull origin master
